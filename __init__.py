da38de84cea169ccc37fe17a0abca9083acb1bc3
Alejandro Perez Malagon <alepmalagon@gmail.com>
2022-02-14 11:31:13 -0500

    parametrizing url of the robot

--- __init__.py
+++ __init__.py
@@ -9,9 +9,10 @@ class MyuraGo(MycroftSkill):
 
     @intent_handler(IntentBuilder('GoIntent').require('GoKeyword'))
     def handle_go_myura(self, message):
+        myura_url = self.settings.get('myura_url')
         payload = json.dumps({'target':[1,0,6], 'planner': False})
-        requests.post('http://10.42.0.146:5000/goto', data = payload)
         self.speak_dialog('go.myura')
+        requests.post(myura_url+'/goto', data = payload)
 
 
 def create_skill():
