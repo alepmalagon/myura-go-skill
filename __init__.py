eb4076ba2dc59d4ecb648b47d88a19c00157c049
Alejandro Perez Malagon <alepmalagon@gmail.com>
2022-02-13 21:12:01 -0500

    adding request

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_handler
-
+import requests
 
 class MyuraGo(MycroftSkill):
     def __init__(self):
@@ -8,7 +8,7 @@ class MyuraGo(MycroftSkill):
 
     @intent_handler(IntentBuilder('GoIntent').require('GoKeyword'))
     def handle_go_myura(self, message):
-
+        requests.post('http://10.42.0.146:5000/goto', data = {'target':'[1,0,6]', 'planner': 'false'})
         self.speak_dialog('go.myura')
 
 
