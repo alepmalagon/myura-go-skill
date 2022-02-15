897dbc6c75b6f25ab00a3f2aa091a201ef8a42b1
Alejandro Perez Malagon <alepmalagon@gmail.com>
2022-02-15 18:29:28 -0500

    a lot of new features

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_handler
+from mycroft.util.parse import extract_datetime, extract_number, extract_numbers
 import requests
 import json
 
@@ -7,13 +8,42 @@ class MyuraGo(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_handler(IntentBuilder('GoIntent').require('GoKeyword'))
+    @intent_handler(IntentBuilder('PositionIntent').require('PositionKeyword'))
     def handle_go_myura(self, message):
         myura_url = self.settings.get('myura_url')
-        payload = json.dumps({'target':[1,0,6], 'planner': False})
+
+        utterance = message.data.get('utterance')
+        position = extract_numbers(utterance)
+        if len(position)<2:
+            self.speak('I need more numbers')
+            return
+        elif len(position)<3:
+            position.append(6)
+
+        payload = json.dumps({'target':position[:3], 'planner': False})
         self.speak_dialog('GoMyura')
         requests.post(myura_url+'/goto', data = payload)
 
+    @intent_handler(IntentBuilder('MoveIntent').require('MoveKeyword'))
+    def handle_go_myura(self, message):
+        myura_url = self.settings.get('myura_url')
+
+        utterance = message.data.get('utterance')
+        first, *middle, last = utterance.split()
+        r = requests.get(myura_url+'/landmarks')
+
+        destination = r[last]
+
+        if not destination:
+            self.speak_dialog("I don't know that place")
+            return
+
+        coords = destination['geometry']['coordinates'][0]
+        coords.append(10)
+
+        payload = json.dumps({'target':coords[:3], 'planner': False})
+        self.speak_dialog('GoMyura')
+        requests.post(myura_url+'/goto', data = payload)
 
 def create_skill():
     return MyuraGo()
