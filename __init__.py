0c3417150e655daa70fc087e440e5fe23b551057
Alejandro Perez Malagon <alepmalagon@gmail.com>
2022-02-16 17:50:23 -0500

    new interactions

--- __init__.py
+++ __init__.py
@@ -3,13 +3,14 @@ from mycroft import MycroftSkill, intent_handler
 from mycroft.util.parse import extract_datetime, extract_number, extract_numbers
 import requests
 import json
+import math
 
 class MyuraGo(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
     @intent_handler(IntentBuilder('PositionIntent').require('PositionKeyword'))
-    def handle_go_myura(self, message):
+    def handle_position_myura(self, message):
         myura_url = self.settings.get('myura_url')
 
         utterance = message.data.get('utterance')
@@ -25,7 +26,7 @@ class MyuraGo(MycroftSkill):
         requests.post(myura_url+'/goto', data = payload)
 
     @intent_handler(IntentBuilder('MoveIntent').require('MoveKeyword'))
-    def handle_go_myura(self, message):
+    def handle_move_myura(self, message):
         myura_url = self.settings.get('myura_url')
 
         utterance = message.data.get('utterance')
@@ -43,6 +44,39 @@ class MyuraGo(MycroftSkill):
             coords.append(10)
         else:
             coords = [0,0,10]
+        payload = json.dumps({'target':coords[:3], 'planner': False})
+        self.speak_dialog('GoMyura')
+        requests.post(myura_url+'/goto', data = payload)
+
+    @intent_handler(IntentBuilder('GoIntent').require('GoKeyword'))
+    def handle_go_myura(self, message):
+        myura_url = self.settings.get('myura_url')
+
+        utterance = message.data.get('utterance')
+        distance = extract_number(utterance)
+        if not distance:
+            self.speak_dialog("give me numbers!")
+            return
+        words = utterance.split()
+        angle = 0.0
+        if 'forward' in words:
+            angle = 0.0;
+        elif 'backwards' in words:
+            angle = 2*math.pi;
+        elif 'left' in words:
+            angle = 3/2*math.pi;
+        elif 'right' in words:
+            angle = 1/2*math.pi;
+        r = requests.get(myura_url+'/odometry')
+        rjson = r.json()
+        theta = rjson["theta"] 
+        x = rjson["x"] 
+        y = rjson["y"] 
+        turn = theta - angle
+        next_x = x + math.cos(turn)*distance
+        next_y = y + math.sin(turn)*distance
+
+        coords = [next_x,next_y,10]
         payload = json.dumps({'target':coords[:3], 'planner': False})
         self.speak_dialog('GoMyura')
         requests.post(myura_url+'/goto', data = payload)
